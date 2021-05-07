65703ddf8d19778b48e725177570c86ed605aaaa
ShaimaaKM <83744361+shaimaa44@users.noreply.github.com>
2021-05-07 08:04:34 +0300

    Website-Shaimaa

---
+++ Website-Shaimaa/PAGES/script1.js
@@ -0,0 +1,28 @@
+let label1 = ['Work', 'Family', 'Self-study', 'Entertainment', 'Relax'];
+let data1 = [35, 15, 20, 15, 15];
+let colors1 = ['#ffcce6', '#1ac6ff', '#b3b3ff', '#85e085', '#ffd480'];
+
+let myChart1 = document.getElementById("myChart").getContext('2d');
+let chart1 = new Chart(myChart1, {
+    type: 'doughnut',
+    data: {
+        labels: label1,
+        datasets: [ {
+            data: data1,
+            backgroundColor: colors1,
+            borderWidth: 1, 
+            borderColor: '#777',
+            hoverBorderWidth: 3,
+            hoverBorderColor: '#000'
+        }]
+    }, 
+    options: {
+        title: {
+            text: "Daily time",
+            display: true
+        }
+    }
+});
+
+
+
